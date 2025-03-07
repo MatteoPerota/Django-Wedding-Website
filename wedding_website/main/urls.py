fb87471b89dbb5c97b8f8093a16085f8e437d11b
Matteo <77929794+MatteoPerota@users.noreply.github.com>
2025-03-06 23:11:59 -0500

    Update urls.py

--- wedding_website/main/urls.py
+++ wedding_website/main/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
+from .views import rsvp_view  # Ensure you're importing the view, not a model
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('rsvp/', views.rsvp, name='rsvp'),
+    path('rsvp/', rsvp_view, name='rsvp'),
 ]
